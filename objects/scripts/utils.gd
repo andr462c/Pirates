5a8342ae480021ff3f94523c758e97d7371fd9bf
Matias Frank Jensen <matias@nuntio.io>
2024-11-16 20:29:42 +0100

    Scene constructor and camera movement

--- objects/scripts/utils.gd
+++ objects/scripts/utils.gd
@@ -23,3 +23,25 @@ static func get_parents(node: Node) -> Array:
 		res.append(p)
 		cur = p
 	return res
+
+
+static func find_child(node: Node, name: String) -> Node:
+	if node.name == name:
+		return node
+	for c in node.get_children():
+		var res = find_child(c, name)
+		if res != null:
+			return res
+	return null
+
+
+static func bounding_box(nodes: Array[Node2D]) -> Array[Vector2]:
+	var top_left = Vector2(1_000_000_000, 1_000_000_000)
+	var bottom_right = Vector2(-1_000_000_000, -1_000_000_000)
+	for n in nodes:
+		var p = n.global_position
+		top_left.x = min(top_left.x, p.x)
+		bottom_right.x = max(bottom_right.x, p.x)
+		top_left.y = min(top_left.y, p.y)
+		bottom_right.y = max(bottom_right.y, p.y)
+	return [top_left, bottom_right]
