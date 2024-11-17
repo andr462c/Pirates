c489c005ebc0662258aa8ae873457ae2b1a1a41b
Asger Phillip Andersen <asger.phillip.andersen@partisia.com>
2024-11-17 10:16:34 +0100

    Fix card selection

--- objects/scenes/card_selection_water.gd
+++ objects/scenes/card_selection_water.gd
@@ -9,8 +9,11 @@ var switch_sound = $UiSwitchSound
 @onready
 var outline = $Card1/Outline
 
-var player1
-var player2
+var p1_modifier
+var p2_modifier
+
+var p1
+var p2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,17 +25,18 @@ func show_cards():
 	$Card1/Area2D.monitoring = true
 	$Card2/Area2D.monitoring = true
 	$Card3/Area2D.monitoring = true
+	selected[0] = false
+	selected[1] = false
+	p1 = get_node("/root/Main/Controller/Players/P0")
+	if has_node("root/Main/Controller/Players/P1"):
+		p2 = get_node("/root/Main/Controller/Players/P1")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("key_select_0"):
-		if player1 == null:
-			player1 = get_node("/root/Main/Controller/Players/P0")
-		confirm_selection(player1)
+		confirm_selection(p1_modifier, 0)
 	elif Input.is_action_just_pressed("key_select_1"):
-		if player2 == null:
-			player2 = get_node("/root/Main/Controller/Players/P1")
-		confirm_selection(player2)
+		confirm_selection(p2_modifier, 1)
 
 
 func _card1_entered(body: Node2D) -> void:
@@ -43,9 +47,11 @@ func _card1_entered(body: Node2D) -> void:
 	$Card2/Outline.visible = false
 	$Card3/Outline.visible = false
 	if body.id == 0:
-		player1 = body
+		p1_modifier = get_child(item_index).get_node("Modifier")
+		p2_modifier = null
 	else:
-		player2 = body
+		p2_modifier = get_child(item_index).get_node("Modifier")
+		p1_modifier = null
 	
 
 func _card2_entered(body: Node2D) -> void:
@@ -56,9 +62,11 @@ func _card2_entered(body: Node2D) -> void:
 	$Card2/Outline.visible = false
 	$Card3/Outline.visible = false
 	if body.id == 0:
-		player1 = body
+		p1_modifier = get_child(item_index).get_node("Modifier")
+		p2_modifier = null
 	else:
-		player2 = body
+		p2_modifier = get_child(item_index).get_node("Modifier")
+		p1_modifier = null
 
 
 func _card3_entered(body: Node2D) -> void:
@@ -69,35 +77,38 @@ func _card3_entered(body: Node2D) -> void:
 	$Card2/Outline.visible = false
 	$Card3/Outline.visible = false
 	if body.id == 0:
-		player1 = body
+		p1_modifier = get_child(item_index).get_node("Modifier")
+		p2_modifier = null
 	else:
-		player2 = body
+		p2_modifier = get_child(item_index).get_node("Modifier")
+		p1_modifier = null
 
 
 func _card_left(bodt: Node2D) -> void:
 	pass
 
-var selected = 0
-var player_who_selected
+var selected = [false, false]
 
-func confirm_selection(player: PlayerBoat):
-	if item_index == -1 || visible == false:
+func confirm_selection(modifier: Node2D, p_id: int):
+	if item_index == -1 || visible == false || modifier == null:
 		return
 	
-	if selected == 1 && player_who_selected == player:
+	if selected[p_id]:
 		return
 	
-	selected += 1
+	selected[p_id] = true
 	
 	print("Selected: ", item_index)
 	power_up_sound.play()
 	get_child(item_index).visible = false
 	get_node("Card%d/Area2D" % (item_index + 1)).monitoring = false
 	
-	get_child(item_index).get_node("Modifier").modify_player(player)
-	player_who_selected = player
+	if p_id == 0:
+		modifier.modify_player(p1)
+	else:
+		modifier.modify_player(p2)
 	
-	if selected < 2:
+	if !selected[1 - p_id] && p1 != null && p2 != null:
 		return
 		
 	for child in get_children():
@@ -114,6 +125,4 @@ func confirm_selection(player: PlayerBoat):
 	if has_node("../ChillMusic"):
 		get_node("../ChillMusic").queue_free()
 	controller.next_level()
-	selected = 0
-	player_who_selected = null
 	
