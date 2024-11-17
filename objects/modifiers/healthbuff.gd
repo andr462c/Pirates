b6e8afe5f54c812d6eb5f7cf69577fbc031a3a8c
Asger Phillip Andersen <asger.phillip.andersen@partisia.com>
2024-11-17 10:26:22 +0100

    huH

--- objects/modifiers/healthbuff.gd
+++ objects/modifiers/healthbuff.gd
@@ -2,3 +2,5 @@ extends Node2D
 
 func modify_player(player) -> void:
 	player.max_health = 20
+	var hp_bar = get_node("/root/Main/Healthbars/P%dHealth" % player.id)
+	hp_bar.max_value = player.max_health
